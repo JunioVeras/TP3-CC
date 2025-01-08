13033ffad8009d31272fb3e36cec30acf7b73e1f
Junio Veras <junioveras127@gmail.com>
2025-01-08 17:09:49 -0300

    ajustes finais

--- runtime/runtime-container/decode_base64.sh
+++ runtime/runtime-container/decode_base64.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ "$PYFILE_OR_ZIP" != "zip" ]; then
+  echo "PYFILE_OR_ZIP não está configurado como 'zip'. Pulando operação de decodificação."
+  exec "$@"
+fi
+
 if [ -z "$ZIP_BASE64" ]; then
   echo "Erro: A variável de ambiente ZIP_BASE64 não está definida."
   exit 1
